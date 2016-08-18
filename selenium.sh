594df80d97dc3741a56f365025a1d16c44458747
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-18 16:14:51 +0530

    Update selenium.sh

--- selenium.sh
+++ selenium.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+!/bin/bash
 
-#CNAME="$1"
-#CID=$(docker inspect   --format {{.Id}} $CNAME)
-#FILE_NAME="AutomationTest.sh"
+CNAME="$1"
+CID=$(docker inspect   --format {{.Id}} $CNAME)
+FILE_NAME="AutomationTest.sh"
 
-#if [ -n "$CID" ] ; then
-#    if [ -f  /var/lib/docker/image/aufs/layerdb/mounts/$CID/mount-id ] ; then
-#        F1=$(cat /var/lib/docker/image/aufs/layerdb/mounts/$CID/mount-id)
-#       d1=/var/lib/docker/aufs/mnt/$F1
-#    fi
-#    if [ ! -d "$d1" ] ; then
-#        d1=/var/lib/docker/aufs/diff/$CID
-#    fi
-#    echo $d1
-#fi
+if [ -n "$CID" ] ; then
+    if [ -f  /var/lib/docker/image/aufs/layerdb/mounts/$CID/mount-id ] ; then
+        F1=$(cat /var/lib/docker/image/aufs/layerdb/mounts/$CID/mount-id)
+       d1=/var/lib/docker/aufs/mnt/$F1
+    fi
+    if [ ! -d "$d1" ] ; then
+        d1=/var/lib/docker/aufs/diff/$CID
+    fi
+    echo $d1
+fi
 
 # Below code will work on node
 
-#cp /var/tmp/$FILE_NAME $d1/var/tmp/
-#docker cp $CNAME:/var/tmp/$FILE_NAME $d1/var/tmp/$FILE_NAME
-#docker exec -it $CNAME /bin/sh -l -c "chmod +x /var/tmp/AutomationTest.sh"
-#docker exec -it $CNAME /bin/sh -l -c "/var/tmp/AutomationTest.sh"
+cp /var/tmp/$FILE_NAME $d1/var/tmp/
+docker cp $CNAME:/var/tmp/$FILE_NAME $d1/var/tmp/$FILE_NAME
+docker exec -it $CNAME /bin/sh -l -c "chmod +x /var/tmp/AutomationTest.sh"
+docker exec -it $CNAME /bin/sh -l -c "/var/tmp/AutomationTest.sh"
 
-#docker cp $CNAME:/root/TestSuite/test_result.log /var/tmp/test_result.log
-#sudo chmod 0777 /var/tmp/test_result.log
+docker cp $CNAME:/root/TestSuite/test_result.log /var/tmp/test_result.log
+sudo chmod 0777 /var/tmp/test_result.log
 
 #echo "FAILED"
 
@@ -31,29 +31,16 @@ result=$(sed '$!d' /var/tmp/test_result.log)
 #echo $result
 if [ "$result" = "OK" ]; 
 then
-  echo "OK1";
+  echo "OK";
 else
   failedTests=$(echo $result| grep -o -E '[0-9]+')
-#perc=$( echo "$failedTests/3" | bc -l)
-#echo "Perci"
-#echo $perc
-#PerTestCasePass=$( echo "$perc * 100" | bc)
-#echo $PerTestCasePass
-#n=$(echo |awk '{ print 80*10.69}')
-#n=$(echo |awk '{ print 100*$perc}')
-#echo "Number"
-#echo $n
-#echo $(($passTestPerc)) | bc
-#echo $(( $failedTests / 3 * 100))
-#if [ $(( $failedTests / 3 * 100)) -gt 10 ];
-#if [ if [ $(( $failedTests * 100 / 3)) -gt 10 ]; -gt 10 ];
   if [ $(( $failedTests * 100 / 3)) -gt 10 ];
   then
-                #echo $result; 
-		echo "FAILED";
+        #echo $result; 
+	echo "FAILED";
   else
    	# echo $result; 
-	echo "OK2";
+	echo "OK";
   fi 
 fi
 
