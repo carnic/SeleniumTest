ab938a5e396c09fdf86a2938f3b1a4393238ae19
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-19 09:50:17 +0530

    Update AutomationTest.sh

--- scripts/AutomationTest.sh
+++ scripts/AutomationTest.sh
@@ -6,7 +6,7 @@ export https_proxy=https://santosh_dhanasure:psl15619%2383dob@ptproxy.persistent
 rm -R /root/TestSuite
 mkdir /root/TestSuite
 touch /root/TestSuite/config.py
-echo "url=$line" > /root/TestSuite/config.py
+echo "url='$line'" > /root/TestSuite/config.py
 apt-get install -y killall
 #apt-get update
 #apt-get install -y firefox xvfb python-pip subversion
