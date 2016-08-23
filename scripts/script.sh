7cc414082e6f0d3d0d4e051a019a956e52979c58
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-23 17:39:52 +0530

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -27,10 +27,10 @@ sed -i -e 's/index.html/index.php/g' /etc/apache2/mods-enabled/dir.conf
 apt-get -y install subversion
 #svn export --username cnp --password psl15619\#83dob http://10.80.34.174:2145/svn/repository/testrepo/UserMgmt.zip /var/tmp/
 
-svn export --non-interactive --trust-server-cert --username santosh_dhanasure@persistent.co.in --password psl15619\#83dob https://svn.persistent.co.in/svn/DevOps_Compt/CarolPereira/CaseStudy/UserMgmt.zip /var/tmp
+#svn export --non-interactive --trust-server-cert --username santosh_dhanasure@persistent.co.in --password psl15619\#83dob https://svn.persistent.co.in/svn/DevOps_Compt/CarolPereira/CaseStudy/UserMgmt.zip /var/tmp
 
-unzip -j /var/tmp/UserMgmt.zip -d /var/www/html
+#unzip -j /var/tmp/UserMgmt.zip -d /var/www/html
 
 apt-get -y install php5-mysql
 
-service apache2 start
+#service apache2 start
