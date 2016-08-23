93360e46ff7a05f56a4a668b92270d882946ac6f
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-23 10:58:04 +0530

    Update db.sh

--- scripts/db.sh
+++ scripts/db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export http_proxy=http://carol_pereira:July13Dday@hjproxy.persistent.co.in:8080
-export https_proxy=https://carol_pereira:July13Dday@hjproxy.persistent.co.in:8080
+export http_proxy=http://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080
+export https_proxy=https://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080
 
 rm /var/lib/apt/lists/lock
 rm /var/cache/apt/archives/lock
