e99e7abfc539c90f1700114b29a158e855b393dd
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-23 17:00:08 +0530

    Update sshSetup.sh

--- scripts/sshSetup.sh
+++ scripts/sshSetup.sh
@@ -4,5 +4,5 @@ apt-get update
 apt-get install -y openssh-server
 sed -i -e 's/PermitRootLogin without-password/#PermitRootLogin without-password/g' /etc/ssh/sshd_config
 sed -i '$ a PermitRootLogin yes' /etc/ssh/sshd_config
-echo -e "pass\npass" | passwd root
+echo "root:pass" | chpasswd
 service ssh restart
