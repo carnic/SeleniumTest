b01b6336819f09b5cc33cf224ae8d753764b0516
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-24 08:50:38 +0530

    Update chefWork.sh

--- chefWork.sh
+++ chefWork.sh
@@ -25,6 +25,6 @@ docker exec -it $CNAME /bin/sh -l -c "/var/tmp/sshSetup.sh"
 CIP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $CNAME)
 DBIP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $DBNAME)
 cd /home/carolnp/carolP/chef-repo
-knife bootstrap $CIP -x root -P pass -N grace123 -r recipe[svnExport] --bootstrap-proxy http://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080
+knife bootstrap $CIP -x root -P pass -N grace3 -r recipe[svnExport] --bootstrap-proxy http://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080
 docker exec -i $CNAME /bin/bash -c "sed -i -e 's/localhost/$DBIP:3306/g' /var/www/html/dbconfig.php"
 docker exec -i $CNAME /bin/bash -c "sed -i -e 's/\"\"/\"root\"/g' /var/www/html/dbconfig.php"
