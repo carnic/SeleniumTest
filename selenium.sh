56e4f1afebab703a6b04fcefadffedff82056fad
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-24 11:11:28 +0530

    Update selenium.sh

--- selenium.sh
+++ selenium.sh
@@ -27,20 +27,20 @@ sudo chmod 0777 /var/tmp/test_result.log
 
 #echo "FAILED"
 
-result=$(sed '$!d' /var/tmp/test_result.log)
+#result=$(sed '$!d' /var/tmp/test_result.log)
 #echo $result
-if [ "$result" = "OK" ]; 
-then
-  echo "OK";
-else
-  failedTests=$(echo $result| grep -o -E '[0-9]+')
-  if [ $(( $failedTests * 100 / 3)) -gt 10 ];
-  then
-        #echo $result; 
-	echo "FAILED";
-  else
+#if [ "$result" = "OK" ]; 
+#then
+#  echo "OK";
+#else
+#  failedTests=$(echo $result| grep -o -E '[0-9]+')
+#  if [ $(( $failedTests * 100 / 3)) -gt 10 ];
+#  then
+#        #echo $result; 
+#	echo "FAILED";
+#  else
    	# echo $result; 
-	echo "OK";
-  fi 
-fi
+#	echo "OK";
+#  fi 
+#fi
 
