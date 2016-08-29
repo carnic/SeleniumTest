bc88f444cff32f2271085e03b834694d9bdc139d
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-29 17:48:05 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -80,7 +80,7 @@ docker exec -it $CNAME /bin/sh -l -c "/var/tmp/$FILE_NAME"
 #dbcontainer=$(docker inspect   --format {{.Id}} $CNA
 
 #echo $DB_C
-dbcontainerip=$(docker inspect -f '{{.NetworkSettings.IPAddress }}' $DB_C)
+#dbcontainerip=$(docker inspect -f '{{.NetworkSettings.IPAddress }}' $DB_C)
 #echo $dbcontainerip
 #docker exec -i $CNAME /bin/bash -c "sed -i -e 's/localhost/$dbcontainerip:3306/g' /var/www/html/dbconfig.php"
 #docker exec -i $CNAME /bin/bash -c "sed -i -e 's/\"\"/\"root\"/g' /var/www/html/dbconfig.php"
