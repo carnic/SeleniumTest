b5904d0b76e3a1d69ce7927369b085206145d54e
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-30 19:23:59 +0530

    Update selenium.sh

--- scripts/selenium.sh
+++ scripts/selenium.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export http_proxy=http://santosh_dhanasure:psl15619%2383dob@ptproxy.persistent.co.in:8080
-export https_proxy=https://santosh_dhanasure:psl15619%2383dob@ptproxy.persistent.co.in:8080
+export http_proxy=http://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080
+export https_proxy=https://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080
 
 apt-get -y update
 apt-get install -y firefox xvfb python-pip subversion
