2d549fbcb33fec02d7afad96567a52a94f0f9d76
Carol Pereira <carolnicolapereira@gmail.com>
2016-08-30 09:47:57 +0530

    Update AutomationTest.sh

--- scripts/AutomationTest.sh
+++ scripts/AutomationTest.sh
@@ -15,6 +15,6 @@ nohup Xvfb :10 -ac &
 export DISPLAY=:10
 firefox --safe-mode &
 svn export --non-interactive --trust-server-cert --username carol_pereira@persistent.co.in --password August23Vm https://svn.persistent.co.in/svn/DevOps_Compt/CarolPereira/CaseStudy/test.py /root/TestSuite/test.py
-python /root/TestSuite/test.py $1
+python /root/TestSuite/test.py
 killall Xvfb
 
