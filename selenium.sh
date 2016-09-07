c427390b403c7702d95b28dce7cab5361449355d
Carol Pereira <carolnicolapereira@gmail.com>
2016-09-07 10:31:27 +0530

    Update selenium.sh

--- selenium.sh
+++ selenium.sh
@@ -1,7 +1,6 @@
 !/bin/bash
 
 CNAME="$1"
-#WANNAME="$2"
 CID=$(docker inspect   --format {{.Id}} $CNAME)
 FILE_NAME="AutomationTest.sh"
 
@@ -17,7 +16,6 @@ if [ -n "$CID" ] ; then
 fi
 
 # Below code will work on node
-#sed -i '2i line="http://10.80.34.148:2392/"' /var/tmp/$FILE_NAME
 url="$3"
 sed -i "2i $url" /var/tmp/$FILE_NAME
 cp /var/tmp/$FILE_NAME $d1/var/tmp/
@@ -27,24 +25,3 @@ docker exec -it $CNAME /bin/sh -l -c "/var/tmp/AutomationTest.sh"
 
 docker cp $CNAME:/root/TestSuite/test_result.log /var/tmp/test_result.log
 sudo chmod 0777 /var/tmp/test_result.log
-#CHostname=$(docker inspect --format '{{ .Config.Hostname }}' $WANNAME)
-#ssh-keygen -R $CHostname
-#echo "FAILED"
-
-#result=$(sed '$!d' /var/tmp/test_result.log)
-#echo $result
-#if [ "$result" = "OK" ]; 
-#then
-#  echo "OK";
-#else
-#  failedTests=$(echo $result| grep -o -E '[0-9]+')
-#  if [ $(( $failedTests * 100 / 3)) -gt 10 ];
-#  then
-#        #echo $result; 
-#	echo "FAILED";
-#  else
-   	# echo $result; 
-#	echo "OK";
-#  fi 
-#fi
-
