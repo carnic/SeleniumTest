355e805ea73e28a70ed4f6e8900615e0366e9666
Carol Pereira <carolnicolapereira@gmail.com>
2016-09-07 11:56:27 +0530

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export http_proxy=http://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080
-export https_proxy=https://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080
+#<<<<<<< uncomment the following line to set proxy >>>>>>>>>>>>>
+#export http_proxy="http://<username>:<password>@<proxy_url>:<port>"
+#export https_proxy="https://<username>:<password>@<proxy_url>:<port>"
 
 rm /var/lib/apt/lists/lock
 rm /var/cache/apt/archives/lock
@@ -22,15 +23,5 @@ apt-get -y install apache2
 apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
 sed -i -e 's/index.html/index.php/g' /etc/apache2/mods-enabled/dir.conf
 
-#nzip -j /var/tmp/app.zip -d /var/www/html
-
 apt-get -y install subversion
-#svn export --username cnp --password psl15619\#83dob http://10.80.34.174:2145/svn/repository/testrepo/UserMgmt.zip /var/tmp/
-
-#svn export --non-interactive --trust-server-cert --username santosh_dhanasure@persistent.co.in --password psl15619\#83dob https://svn.persistent.co.in/svn/DevOps_Compt/CarolPereira/CaseStudy/UserMgmt.zip /var/tmp
-
-#unzip -j /var/tmp/UserMgmt.zip -d /var/www/html
-
 apt-get -y install php5-mysql
-
-#service apache2 start
