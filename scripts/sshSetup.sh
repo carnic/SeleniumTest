535e17d864b2c19157f4443804956a47634af87e
Carol Pereira <carolnicolapereira@gmail.com>
2016-09-07 11:57:06 +0530

    Update sshSetup.sh

--- scripts/sshSetup.sh
+++ scripts/sshSetup.sh
@@ -1,5 +1,7 @@
-export http_proxy="http://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080"
-export https_proxy="https://carol_pereira:August23Vm@hjproxy.persistent.co.in:8080"
+#<<<<<<< uncomment the following line to set proxy >>>>>>>>>>>>>
+#export http_proxy="http://<username>:<password>@<proxy_url>:<port>"
+#export https_proxy="https://<username>:<password>@<proxy_url>:<port>"
+
 apt-get update
 apt-get install -y openssh-server
 sed -i -e 's/PermitRootLogin without-password/#PermitRootLogin without-password/g' /etc/ssh/sshd_config
